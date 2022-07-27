438ffdc703908f53a11b9e1c8ab23d6b0f8c6cdd
Mark <markwilkins86@gmail.com>
2022-07-27 12:28:56 -0400

    Main Set

--- client/src/ReminderCard.js
+++ client/src/ReminderCard.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import EditForm from "./EditForm";
 
-import Card from "react-bootstrap/Card";
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { createTheme } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
@@ -22,16 +24,14 @@ function ReminderCard({
   time,
   incomplete,
   address,
-  category, 
+  category,
   reminder,
   reminders,
   setReminders,
   setUser,
-})
-
-{
+}) {
   function handleDelete() {
-    console.log("delete called");
+    // console.log("delete called");
     fetch(`/my-reminders/${reminder.id}`, { method: "DELETE" }).then((res) =>
       res.json().then((data) => {
         setReminders(reminders.filter((rem) => rem.id !== data.id));
@@ -41,7 +41,7 @@ function ReminderCard({
 
   const [showEditForm, setShowEditForm] = useState(false);
   const [complete, setComplete] = useState(false);
-  const [updateTime, setUpdateTime] = useState("")
+  const [updateTime, setUpdateTime] = useState("");
 
   function editForm(e) {
     setShowEditForm(!showEditForm);
@@ -51,11 +51,10 @@ function ReminderCard({
     setComplete(!complete);
   }
 
-
-  const changeTime = () => {
+  const adjustTime = () => {
     let time = reminder.time;
+    // console.log(time);
     time = time.split(":");
-
 
     let hours = Number(time[0]);
     let minutes = Number(time[1]);
@@ -73,81 +72,80 @@ function ReminderCard({
     timeValue += minutes < 10 ? ":0" + minutes : ":" + minutes;
     timeValue += hours >= 12 ? " P.M." : " A.M.";
 
-    
-    setUpdateTime(timeValue)
-  }
+    setUpdateTime(timeValue);
+  };
 
   useEffect(() => {
-    changeTime()
-    console.log(updateTime)
-  }, [])
+    adjustTime();
+    console.log(updateTime);
+  }, []);
 
-
-  }
-    
   return (
-    <div>
-      <Card border="secondary" style={{ width: "18rem" }}>
-        <Card.Body className={!complete ? "incomplete" : "complete"}>
-          <span class="emoji">📍</span>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>{address}</Card.Text>
-          <Card.Text>{date}</Card.Text>
-
-          <Card.Text>{updateTime}</Card.Text>
-
-          <style type="text/css">
-            {`
+    <Card Card sx={{ minWidth: 275 }}>
+      <CardContent className={!complete ? "incomplete" : "complete"}>
+        <span class="emoji">📍</span>
+        <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+          <strong>{title}</strong>
+        </Typography>
+        <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+          <strong>{address}</strong>
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {date}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {updateTime}
+        </Typography>
+        <style type="text/css">
+          {`
     .btn-btn {
       color: black;
     `}
-          </style>
-
-          {/* Delete Button */}
-          <Button
-            onClick={() => handleDelete()}
-            variant="btn"
-            theme={theme}
-            sx={{ boxShadow: 3 }}
-            style={{ backgroundColor: "#FF914D" }}
-          >
-            💣
-          </Button>
-          {/* Edit Form */}
-          <Button
-            onClick={editForm}
-            variant="btn"
-            theme={theme}
-            sx={{ boxShadow: 3 }}
-            style={{ backgroundColor: "#FF914D" }}
-          >
-            {showEditForm ? "Hide Edit" : "Edit"}
-          </Button>
-          <br />
-          <br />
-          <br />
-          {showEditForm ? (
-            <EditForm
-              user={user}
-              reminder={reminder}
-              reminders={reminders}
-              setReminders={setReminders}
-            />
-          ) : null}
-
-          {/* Complete Button */}
-          <Button
-            onClick={handleComplete}
-            variant="btn"
-            theme={theme}
-            sx={{ boxShadow: 3 }}
-            style={{ backgroundColor: "#FF914D" }}
-          >
-            {complete ? "✔" : "Done!"}
-          </Button>
-        </Card.Body>
-      </Card>
-    </div>
+        </style>
+        <br />
+        {/* Delete Button */}
+        <Button
+          onClick={() => handleDelete()}
+          variant="btn"
+          theme={theme}
+          sx={{ boxShadow: 3 }}
+          style={{ backgroundColor: "#FF914D" }}
+        >
+          💣
+        </Button>
+        {/* Edit Form */}
+        <Button
+          onClick={editForm}
+          variant="btn"
+          theme={theme}
+          sx={{ boxShadow: 3 }}
+          style={{ backgroundColor: "#FF914D" }}
+        >
+          {showEditForm ? "Hide Edit" : "Edit"}
+        </Button>
+        <br />
+        <br />
+        <br />
+        {showEditForm ? (
+          <EditForm
+            user={user}
+            reminder={reminder}
+            reminders={reminders}
+            setReminders={setReminders}
+          />
+        ) : null}
+        {/* Complete Button */}
+        <Button
+          onClick={handleComplete}
+          variant="btn"
+          theme={theme}
+          sx={{ boxShadow: 3 }}
+          style={{ backgroundColor: "#FF914D" }}
+        >
+          {complete ? "✔" : "Done!"}
+        </Button>
+      </CardContent>
+    </Card>
   );
 }
 
