8b6bf493c97035b0ac0abcba9e46e18e4e3f1d5c
Mark <markwilkins86@gmail.com>
2022-08-08 12:09:15 -0400

    working on deploying

--- client/src/PushNotifications.js
+++ client/src/PushNotifications.js
@@ -1,21 +1,21 @@
-function isPushNotificationSupported() {
-  return "serviceWorker" in navigator && "PushManager" in window;
-}
+// function isPushNotificationSupported() {
+//   return "serviceWorker" in navigator && "PushManager" in window;
+// }
 
-async function askUserPermission() {
-  return await Notification.requestPermission();
-}
+// async function askUserPermission() {
+//   return await Notification.requestPermission();
+// }
 
-function registerServiceWorker() {
-  return navigator.serviceWorker.register("/sw.js");
-}
+// function registerServiceWorker() {
+//   return navigator.serviceWorker.register("/sw.js");
+// }
 
-async function createNotificationSubscription() {
-  //wait for service worker installation to be ready
-  const serviceWorker = await navigator.serviceWorker.ready;
-  // subscribe and return the subscription
-  return await serviceWorker.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: pushServerPublicKey,
-  });
-}
+// async function createNotificationSubscription() {
+//   //wait for service worker installation to be ready
+//   const serviceWorker = await navigator.serviceWorker.ready;
+//   // subscribe and return the subscription
+//   return await serviceWorker.pushManager.subscribe({
+//     userVisibleOnly: true,
+//     applicationServerKey: pushServerPublicKey,
+//   });
+// }
